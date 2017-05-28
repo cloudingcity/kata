fa5fe729b0b48c4ebdb38b7b62e40a95fc6fc091
clouding <ghost0436@gmail.com>
2017-05-28 19:05:31 +0800

    Cart #1 Second Test
    Test Product __construct() and getter()

--- src/Cart/Product.php
+++ src/Cart/Product.php
@@ -4,23 +4,29 @@ namespace Clouding\kata\Cart;
 
 class Product implements Productable
 {
+    private $name = '';
+    private $price = 0;
+    private $tag = '';
+
     public function __construct($name, $price, $tag)
     {
-        
+        $this->name = $name;
+        $this->price = $price;
+        $this->tag = $tag;
     }
 
     public function getName()
     {
-        
+        return $this->name;
     }
 
     public function getPrice()
     {
-        
+        return $this->price;
     }
 
     public function getTag()
     {
-        
+        return $this->tag;
     }
 }
--- tests/Cart/ProductTest.php
+++ tests/Cart/ProductTest.php
@@ -5,5 +5,11 @@ use PHPUnit\Framework\TestCase;
 
 class ProductTest extends TestCase
 {
-
+    public function testProductConstructorAndGetter()
+    {
+        $product = new Product('Product 1', 245, 'R');
+        $this->assertEquals('Product 1', $product->getName());
+        $this->assertEquals(245, $product->getPrice());
+        $this->assertEquals('R', $product->getTag());
+    }
 }
