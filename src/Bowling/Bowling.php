31c94e74db6f2c71dec43ac10edb26d4f8f0defb
clouding <ghost0436@gmail.com>
2017-05-30 22:12:56 +0800

    Bowling #2 Refactor

--- src/Bowling/Bowling.php
+++ src/Bowling/Bowling.php
@@ -4,15 +4,24 @@ namespace Clouding\kata\Bowling;
 
 class Bowling
 {
-    protected $score = 0;
+    protected $rolls = [];
+    protected $rollIndex = 0;
 
     public function roll($pins)
     {
-        $this->score += $pins;
+        $this->rolls[$this->rollIndex] = $pins;
+        $this->rollIndex++;
     }
 
     public function score(): int
     {
-        return $this->score;
+        $score = 0;
+        $i = 0;
+        for ($frame = 0; $frame < 10; $frame++) {
+            $score += $this->rolls[$i] + $this->rolls[$i + 1];
+            $i += 2;
+        }
+
+        return $score;
     }
 }
