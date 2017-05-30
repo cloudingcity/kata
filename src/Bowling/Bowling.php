96a5dd94e918ed8a47746b1740556f9e98d85119
clouding <ghost0436@gmail.com>
2017-05-30 22:21:26 +0800

    Bowling #3 Green testScore14
    前兩次擊倒2 8瓶，得一個 Spare，再擊倒2瓶
    然後全部洗溝，得分 10 + 2 + 2(bonus) = 14分

--- src/Bowling/Bowling.php
+++ src/Bowling/Bowling.php
@@ -18,7 +18,11 @@ class Bowling
         $score = 0;
         $i = 0;
         for ($frame = 0; $frame < 10; $frame++) {
-            $score += $this->rolls[$i] + $this->rolls[$i + 1];
+            if ($this->rolls[$i] + $this->rolls[$i + 1] == 10) {
+                $score += 10 + $this->rolls[$i + 2];
+            } else {
+                $score += $this->rolls[$i] + $this->rolls[$i + 1];
+            }
             $i += 2;
         }
 
